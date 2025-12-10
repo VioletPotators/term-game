1998fa2326918e8a5219ce0a4bd2d5c6c0b09af3
VioletPotators <arturbesen34@gmail.com>
2025-12-10 20:08:49 -0300

    Enhance word guessing logic in gamelogic.py by converting input words to lowercase, implementing feedback for correct and incorrect letters, and displaying the final word after attempts. The correct word is now hardcoded for testing purposes.

--- backend/gamelogic.py
+++ backend/gamelogic.py
@@ -5,14 +5,15 @@ with open(r'C:\Users\Artur Besen\Desktop\term-game\term-game\backend\word-list\w
     words2 = file.readlines()
     words = []
     for word in words2:
-        words.append(word.strip())
+        words.append(word.strip().lower())
 
 
 
-correct_word = random.choice(words)
-remaining_tries = 6 
+correct_word = "oucas"#random.choice(words)
+remaining_tries = 6     
+print("Term game, guess the word")
 while remaining_tries > 0:
-    print("Term game, guess the word")
+
     print(f"Remaining tries: {remaining_tries}")
     attempt = input("type the word: ")
     if len(attempt) != 5:
@@ -24,7 +25,46 @@ while remaining_tries > 0:
         continue
 
     if attempt == correct_word:
-        print("Correct answer")
+        print(f"Correct answer")
+        break
     else:
-        remaining_tries -= 1
+        attempt_letters = list[str](attempt)
+        correct_word_letters = list(correct_word)
 
+
+        amount_correct = {}
+        for letter in correct_word_letters:
+            if letter not in amount_correct:
+                amount_correct[letter] = 0
+            amount_correct[letter] += 1
+
+        final_check = {}
+        for i, letter in enumerate[str](attempt_letters):
+            if letter == correct_word_letters[i]:
+                final_check[letter + str(i)] = "green"
+                amount_correct[letter] -= 1
+
+                if amount_correct[letter] < 0:
+                    for letter_index, color in final_check.items():
+                        letter_inside = letter_index[0]
+                        index = letter_index[1]
+                        if letter == letter_inside and color == "yellow":
+                            final_check[letter_index] = "none"
+                            amount_correct[letter] += 1
+
+                continue
+            if letter in correct_word_letters and amount_correct[letter] > 0:
+                final_check[letter + str(i)] = "yellow"
+
+                amount_correct[letter] -= 1
+                continue
+            final_check[letter + str(i)] = "none"
+
+
+        print("".join(final_check))
+        remaining_tries -= 1
+        
+
+print(f"The word was: {correct_word}")
+
+ 
